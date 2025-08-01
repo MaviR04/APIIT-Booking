b97817581295d5e048d38de60942faaa462ea3de
MaviR04 <mavirodrigo4@gmail.com>
2025-08-01 15:43:53 +0530

    yippe

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-<<<<<<< HEAD
-
-
-function App() {
-
-
-
-  return (
-    <>
-    
-
-    </>
-  )
-=======
 import React from "react";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
   return <LoginPage />;
->>>>>>> c2d2fbc099f09d8ed430d41b5c37599b5665e559
 }
 
 export default App;
